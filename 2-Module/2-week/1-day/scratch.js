0a4e8114af4a578661a6bedf9a968f2e6fe2cf20
philling83 <philipling83@gmail.com>
2023-06-19 18:28:29 -0400

    Update scratch

--- 2-Module/2-week/1-day/scratch.js
+++ 2-Module/2-week/1-day/scratch.js
@@ -1,1 +1,24 @@
+class Test {
+    testFunc() {
+        console.log("this value: ", this);
+        console.log(`this is equal to test1: ${this === test1}`);
+    }
+}
 
+const test1 = new Test();
+// Calling the method on the instance
+// test1.testFunc(); // true
+
+// Calling the method outside the class
+// const test1Func = test1.testFunc;
+// console.log(test1Func, 14);
+// test1Func(); // false
+
+// // Calling the method as a callback
+// setTimeout(test1.testFunc, 1000); // 
+
+// Using this in Function Declaration Syntax
+function someFunc() {
+    console.log(this);
+}
+someFunc(); // 
